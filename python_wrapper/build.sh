20fc16a7a8351866dbaabeeafe7a89e6431a8e92
Jacopo Panerati <jacopo.panerati@utoronto.ca>
2022-08-15 17:19:55 -0400

    Update build.sh

--- python_wrapper/build.sh
+++ python_wrapper/build.sh
@@ -3,5 +3,5 @@ cd $SCRIPTPATH
 rm -rf build
 rm _cffirmware.cpython-39-x86_64-linux-gnu.so cffirmware_wrap.c cffirmware.py
 export CSW_PYTHON=python3
-make
+make CC=/usr/local/bin/gcc-12
 python3 -m pip install -e .
